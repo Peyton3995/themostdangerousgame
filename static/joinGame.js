42e43d179a38e31a6d9ef40080a1e4cd55dc3da9
Peyton <peyton3995@outlook.com>
2025-11-08 00:13:53 -0500

    make cloeset point visible for testing reasons

--- static/joinGame.js
+++ static/joinGame.js
@@ -188,6 +188,6 @@ function findNearestPoint() {
     console.log(closestPoint)
 
     if(closestPoint.length > 0) {
-        document.getElementById('test').innerHTML = closestPoint
+        document.getElementById('test').innerHTML = closestPoint.toString()
     }
 }
